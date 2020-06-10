9d7ce981b24c626573b3bb1597e358efacc175c0
McKenzy1971 <61012497+McKenzy1971@users.noreply.github.com>
2020-06-10 19:58:12 +0200

    Edit XML Serializer

---
+++ SoftwareBase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+using System.Runtime.Serialization;
+using System.IO;
+
+namespace SoftwareBase
+{
+    public static class XmlSerialiazer<T>
+    {
+        public static XmlSerializer xmlSerializer = new XmlSerializer(typeof(T));
+
+        public static void Serialize(string path, Object obj)
+        {
+            using (Stream s = File.OpenWrite(path))
+                xmlSerializer.Serialize(s, obj);
+        }
+
+        public static Object[] Deserialize(string path)
+        {
+            using (Stream s = File.OpenRead(path))
+            {
+                Object[] objects = (Object[])xmlSerializer.Deserialize(s);
+                return objects;
+            }
+        }
+    }
+}
