4a5e800f228bc36f2a171709960b2825c45d48ec
McKenzy1971 <61012497+McKenzy1971@users.noreply.github.com>
2020-06-10 21:17:47 +0200

    Added Serialize<T> xmlSerializer(typeof(T));

---
+++ SoftwareBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.Serialization;
+using System.Xml.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace SoftwareBase
+{
+    public static class Serialize<T>
+    {
+        public static XmlSerializer xmlSerializer = new XmlSerializer(typeof(T));
+    }
+}
