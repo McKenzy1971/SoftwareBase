7c60adfdfc0b0778dd25ae10dc3abdfff9fcc0ff
McKenzy1971 <61012497+McKenzy1971@users.noreply.github.com>
2020-06-12 15:39:22 +0200

    Added DelegateCommand<T>

--- ViewModelBase.cs
+++ ViewModelBase.cs
@@ -1,8 +1,13 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using System.Windows.Input;
 
 namespace SoftwareBase.ViewModelBase
 {
+    /// <summary>
+    /// Base class of all ViewModels
+    /// </summary>
     public abstract class ViewModelBase : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
@@ -11,5 +16,42 @@ namespace SoftwareBase.ViewModelBase
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
         public Folder folder = new Folder();
+    }
+
+    public class DelegateCommand<T> : ICommand
+    {
+        #region constructos
+
+        public DelegateCommand(Action<T> executeHdl, Predicate<T> canExecuteHdl)
+        {
+            this.canExecuteHdl = canExecuteHdl;
+            if (this.canExecuteHdl == null)
+                throw new ArgumentNullException("executeHdl", "Please specifiy the command");
+            this.executeHdl = executeHdl;
+        }
+        #endregion
+
+        private Predicate<T> canExecuteHdl { get; set; }
+        private Action<T> executeHdl { get; set; }
+
+        public event EventHandler CanExecuteChanged;
+
+        #region Methods
+
+        public void RaiseCanExecuteChanged()
+        {
+            this.CanExecuteChanged?.Invoke(this, null);
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return canExecuteHdl == null || canExecuteHdl((T)parameter) == true;
+        }
+
+        public void Execute(object parameter)
+        {
+            executeHdl((T)parameter);
+        }
+        #endregion
     }
 }
