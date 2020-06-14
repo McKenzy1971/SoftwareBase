2e06da7d05c98efb7de06ab2ec83ea185a5df268
McKenzy1971 <61012497+McKenzy1971@users.noreply.github.com>
2020-06-14 12:10:57 +0200

    Finished Serialize<T> for Testing

--- Serialize.cs
+++ Serialize.cs
@@ -3,15 +3,29 @@ using System.Xml.Serialization;
 
 namespace SoftwareBase.Serialize
 {
+    /// <summary>
+    /// Saves and loads data
+    /// </summary>
+    /// <typeparam name="T">Type of data</typeparam>
     public static class Serialize<T>
     {
-        public static XmlSerializer xmlSerializer = new XmlSerializer(typeof(T));
+        private static XmlSerializer xmlSerializer = new XmlSerializer(typeof(T));
+        /// <summary>
+        /// Creates or Overrides data
+        /// </summary>
+        /// <param name="obj">Datas to save</param>
+        /// <param name="path">Path of datas</param>
         public static void SaveData(T obj, string path)
         {
-            using (Stream s = File.OpenWrite(path))
+            using (Stream s = File.Create(path))
                 xmlSerializer.Serialize(s, obj);
         }
-        public static T LoadData(T obj, string path)
+        /// <summary>
+        /// Loads data
+        /// </summary>
+        /// <param name="path">Path of data</param>
+        /// <returns>T</returns>
+        public static T LoadData(string path)
         {
             T result;
             using (Stream s = File.OpenRead(path))
