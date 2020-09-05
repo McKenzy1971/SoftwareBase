b8ceaab6ec5687173f0aa765d24ad707a57209f9
McKenzy1971 <61012497+McKenzy1971@users.noreply.github.com>
2020-09-06 00:14:34 +0200

    Add Refresh and task service

---
+++ Services/RefreshService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace WorkManager.Services
+{
+    /// <summary>
+    /// RefreshService
+    /// </summary>
+    public static class RefreshService
+    {
+        /// <summary>
+        /// Refresh event
+        /// </summary>
+        public static event EventHandler RefreshEvent;
+
+        /// <summary>
+        /// Invokes Refresh event if it's not null
+        /// </summary>
+        /// <param name="sender">Object that called this method</param>
+        public static void Refresh(object sender)
+        {
+            RefreshEvent?.Invoke(sender, EventArgs.Empty);
+        }
+    }
+}
---
+++ Services/TaskService.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SoftwareBase.Services
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class TaskService
+    {
+        private static HashSet<Task> tasks;
+        private static readonly object locker = new object();
+
+        /// <summary>
+        /// Add task
+        /// </summary>
+        /// <param name="t">Task</param>
+        public static void Add(Task t)
+        {
+            lock (locker)
+                tasks.Add(t);
+        }
+
+        /// <summary>
+        /// Remove task
+        /// </summary>
+        /// <param name="t">Task</param>
+        public static void Remove(Task t)
+        {
+            lock (locker)
+                tasks.Remove(t);
+        }
+
+        /// <summary>
+        /// Remove every task thats != Status.Running || Status.WaitingForActivation
+        /// </summary>
+        public static void CleanUp()
+        {
+            tasks.RemoveWhere((t) => t.Status != TaskStatus.Running || t.Status != TaskStatus.WaitingForActivation);
+        }
+
+        /// <summary>
+        /// Waits for all tasks
+        /// </summary>
+        public static void WaitForTasks()
+        {
+            Task.WaitAll(tasks.ToArray());
+            TaskService.CleanUp();
+        }
+    }
+}
